284e2f3d328770f3c2ae3d93306310591b091c7f
Durgesh <durgeshmali5824@gmail.com>
2024-03-28 01:42:28 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Wild_Animal_Detection_System"
+include(":app")

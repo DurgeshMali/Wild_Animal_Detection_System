451231d86b35fc0281f4b03330c6384113855570
Durgesh <durgeshmali5824@gmail.com>
2024-03-30 03:35:48 +0530

    hii

--- app/src/main/java/com/example/wild_animal_detection_system/LoginActivity.java
+++ app/src/main/java/com/example/wild_animal_detection_system/LoginActivity.java
@@ -4,25 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
-
+    EditText phoneno, password;
+    Button loginButton,signUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        View loginButton = findViewById(R.id.button2);
+        phoneno = findViewById(R.id.editTextText);
+        password = findViewById(R.id.editTextPassword);
+
+        loginButton = findViewById(R.id.button2);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(TextUtils.isEmpty(phoneno.getText().toString())) {
+                    phoneno.setError("Please Enter Phone no.");
+                    return;
+                }
+                if(TextUtils.isEmpty(password.getText().toString())) {
+                    password.setError("Please enter the Name");
+                    return;
+                }
+                Toast.makeText(LoginActivity.this, "Login Successfully", Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
                 startActivity(intent);
             }
         });
 
-        View signUp = findViewById(R.id.button_sign_up);
+        signUp = findViewById(R.id.button_sign_up);
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/example/wild_animal_detection_system/SingupActivity.java
+++ app/src/main/java/com/example/wild_animal_detection_system/SingupActivity.java
@@ -4,19 +4,46 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SingupActivity extends AppCompatActivity {
-
+    EditText name, password, repassword, phonenno;
+    Button signUpButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_singup);
 
-        View signUpButton = findViewById(R.id.button2);
+        name = findViewById(R.id.editTextText3);
+        phonenno = findViewById(R.id.editTextText2);
+        password = findViewById(R.id.editTextText);
+        repassword = findViewById(R.id.editTextPassword);
+
+        signUpButton = findViewById(R.id.button2);
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(TextUtils.isEmpty(name.getText().toString())) {
+                    name.setError("Please enter the Name");
+                    return;
+                }
+                if(TextUtils.isEmpty(phonenno.getText().toString())) {
+                    phonenno.setError("Please Enter Phone no.");
+                    return;
+                }
+                if(TextUtils.isEmpty(password.getText().toString())) {
+                    password.setError("Please enter the Name");
+                    return;
+                }
+                if(TextUtils.isEmpty(repassword.getText().toString())) {
+                    repassword.setError("Please Enter Phone no.");
+                    return;
+                }
+                Toast.makeText(SingupActivity.this, "Singup Successfully", Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(SingupActivity.this, HomeActivity.class);
                 startActivity(intent);
             }

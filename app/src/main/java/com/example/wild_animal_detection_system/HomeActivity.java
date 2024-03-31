5164a400756a31454f7a533ad47a575c6d9e3c13
Durgesh <durgeshmali5824@gmail.com>
2024-03-31 23:41:50 +0530

    hii

--- app/src/main/java/com/example/wild_animal_detection_system/HomeActivity.java
+++ app/src/main/java/com/example/wild_animal_detection_system/HomeActivity.java
@@ -1,14 +1,18 @@
 package com.example.wild_animal_detection_system;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
 
     TextView userName;
+    CardView myProfile, language, liveFeed, customizeCamera, contactUs, help;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,14 @@ public class HomeActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String nameUser = intent.getStringExtra("userName");
         userName.setText("Welcome "+nameUser);
+
+        myProfile = findViewById(R.id.CardView1);
+        language = findViewById(R.id.CardView2);
+        liveFeed = findViewById(R.id.CardView3);
+        customizeCamera = findViewById(R.id.CardView4);
+        contactUs = findViewById(R.id.CardView5);
+        help = findViewById(R.id.CardView6);
+
     }
 
 
